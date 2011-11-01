a4e7df9cdc157a5e7d29f7aed73712a2e862d881
Ben Shippee <ben.shippee@gmail.com>
2011-11-01 18:31:33 -0400

    Added terrain based movement for player, fixed terrain to be collidable

--- player.py
+++ player.py
@@ -28,49 +28,74 @@ class Player(DirectObject):
         self.smg_burst_count = 3
         self.smg_fire_rate = 5
         self.smg_fire_counter = 0
-        self.actor = Actor("models/ralph",
-                            {"run":"models/ralph-run",
-                             "walk":"models/ralph-walk"})
+        self.actor = Actor("models/player")
         self.actor.reparentTo(render)
         self.actor.setScale(0.2)
+        self.actor.setH(-90)
         self.actor.setPos(game.player_start)
         
-        camera.setPos(self.actor.getX(),self.actor.getY()+10,7)
-        camera.lookAt(self.actor)
+        camera.setPosHpr(self.actor.getX()+20,self.actor.getY()+10,4,0,0,0)
+        #camera.lookAt(self.actor)
         
         self.selected_weapon = "SMG"
+        
+        # Add Movement Task
         taskMgr.add(self.move, "PlayerMove", extraArgs= [game])
+        
+        # Add Rotate Task
+        taskMgr.add(self.rotate, "PlayerRotate", extraArgs=[game])
         
     def take_damage(self, damage):
         self.health -= damage
         if self.health <= 0:
             self.die()
+            
+    def select_weapon(self, weapon):
+        self.selected_weapon = weapon
     
     def move(self, game):
+        MOVE = False
         if not game.paused:
-            camera.lookAt(self.actor)
+            self.player_start_pos = self.actor.getPos()
             if game.keyMap["left"]:
-                self.actor.setX(self.actor,  25 * globalClock.getDt())
-            if game.keyMap["right"]:
-                self.actor.setX(self.actor, - 25 * globalClock.getDt())
-            if game.keyMap["forward"]:
-                self.actor.setY(self.actor, - 25 * globalClock.getDt())
-            if game.keyMap["back"]:
                 self.actor.setY(self.actor,  25 * globalClock.getDt())
+            if game.keyMap["right"]:
+                self.actor.setY(self.actor, - 25 * globalClock.getDt())
+            if game.keyMap["forward"]:
+                MOVE = True
+                self.actor.setX(self.actor,  25 * globalClock.getDt())
+            if game.keyMap["back"]:
+                self.actor.setX(self.actor, - 25 * globalClock.getDt())
+            
+            # Check for terrain collisions
+            game.cTrav.traverse(render)
+            
+            # Now update the player's Z coordinate, or don't move at all
+            entries = []
+            for i in range(game.player_cghandler.getNumEntries()):
+                entry = game.player_cghandler.getEntry(i)
+                entries.append(entry)
+            entries.sort(lambda x,y: cmp(y.getSurfacePoint(render).getZ(), x.getSurfacePoint(render).getZ()))
+            if (len(entries)>0) and (entries[0].getIntoNode().getName() == "terrain"):
+                self.actor.setZ(entries[0].getSurfacePoint(render).getZ()+4)
+            else:
+                if MOVE:
+                    print "Cant move"
+                self.actor.setPos(self.player_start_pos)
                 
             # Basic Camera Repositioning, need to tweak.
-            camvec = self.actor.getPos() - base.camera.getPos()
-            camvec.setZ(0)
-            camdist = camvec.length()
-            camvec.normalize()
-            if (camdist > 10.0):
-                camera.setPos(camera.getPos() + camvec*(camdist-10))
-                camdist = 10.0
-            if (camdist < 5.0):
-                camera.setPos(camera.getPos() - camvec*(5-camdist))
-                camdist = 5.0
-                
+            camera.setPosHpr(self.actor.getX(),self.actor.getY()+10,self.actor.getZ()+3,self.actor.getH(),0,0)
+            camera.lookAt(self.actor)
         return Task.cont
+    
+    def rotate(self, game):
+        if not game.paused:
+            if self.selected_weapon is "SMG" or "Shotgun":
+
+                
+                
+                camera.setPosHpr(self.actor.getX(),self.actor.getY()+10,3,0,0,0)
+                camera.lookAt(self.actor)
     
     def die(self):
         pass
