de6253a103174728f40721090c5ee036a4806e4c
Ben Shippee <ben.shippee@gmail.com>
2011-11-05 17:57:30 -0300

    Update game.py

--- game.py
+++ game.py
@@ -6,13 +6,11 @@ from direct.interval.IntervalGlobal import * #for compound intervals
 from direct.task import Task #for update functions
 from direct.gui.DirectGui import * #for buttons and stuff
 from panda3d.ai import * # AI logic
-from pandac.PandaModules import Vec3
 import sys, math, random
 
 import player
 import enemies
 import explosions
-
 
 
 class World(DirectObject): #subclassing here is necessary to accept events
@@ -21,15 +19,16 @@ class World(DirectObject): #subclassing here is necessary to accept events
         #turn off mouse control, otherwise camera is not repositionable
         base.disableMouse()
         #camera.setPosHpr(0, -15, 7, 0, -15, 0)
-        self.loadModels()
         self.setupLights()
-        self.setupCollisions()
         render.setShaderAuto() #you probably want to use this
         
+        
         # Mapping some keys
-        self.keyMap = {"left":0, "right":0, "forward":0, "back":0, "fire":False}
+        self.keyMap = {"left":0, "right":0, "forward":0, "back":0, "shoot":0}
         self.accept("escape", self.pause)
-        self.accept("space", self.boom)
+        self.accept("space", self.setKey, ["shoot", 1])
+        self.accept("space-up",self.setKey, ["shoot", 0])
+        self.accept("e", self.create_explosion)
         self.accept("l", self.toggle_light)
         self.accept("1", self.setSMG)
         self.accept("2", self.setShotgun)
@@ -38,12 +37,10 @@ class World(DirectObject): #subclassing here is necessary to accept events
         self.accept("d", self.setKey, ["right", 1])
         self.accept("a", self.setKey, ["left", 1])
         self.accept("s", self.setKey, ["back",1])
-        self.accept("space", self.setKey, ["fire", True])
         self.accept("w-up", self.setKey, ["forward", 0])
         self.accept("d-up", self.setKey, ["right", 0])
         self.accept("a-up", self.setKey, ["left", 0])
         self.accept("s-up", self.setKey, ["back", 0])
-        self.accept("space-up", self.setKey, ["fire", False])
 
         # Empty lists to track stuff
         self.enemies = []
@@ -51,16 +48,16 @@ class World(DirectObject): #subclassing here is necessary to accept events
         self.mortars = []
         
         # Find the start position
-        self.player_start = (0,0,0)
+        self.player_start = (0,0,20)
         # Make a player object
         self.player = player.Player(self)
         self.count = 0
         
         
         #Load Environment
-        self.environ = loader.loadModel("models/ground")      
+        self.environ = loader.loadModel("models/terrain")      
         self.environ.reparentTo(render)
-        self.environ.setScale(0.2)
+        self.environ.setScale(0.66)
         self.environ.setPos(0,0,0)
         
         # Setup the collision detection rays
@@ -69,7 +66,7 @@ class World(DirectObject): #subclassing here is necessary to accept events
         
         # Player Rays
         self.player_cgray = CollisionRay()
-        self.player_cgray.setOrigin(0,0,80)
+        self.player_cgray.setOrigin(0,0,100)
         self.player_cgray.setDirection(0,0,-1)
         self.player_cgcol = CollisionNode("player_gray")
         self.player_cgcol.addSolid(self.player_cgray)
@@ -91,23 +88,21 @@ class World(DirectObject): #subclassing here is necessary to accept events
         self.cghandler = CollisionHandlerQueue()
         self.cTrav.addCollider(self.cgcolnp, self.cghandler)
 
-        self.player_cgcolnp.show()
-        self.cgcolnp.show()
-        self.cTrav.showCollisions(render)
-     
+        
         self.paused = False
         self.setAI()
         
-
-        for i in range(5):	
+        for i in range(5):    
             self.newEnemy = enemies.Enemy1(self)	
             self.enemies.append(self.newEnemy)
             self.AIworld.addAiChar(self.newEnemy.setupAI(self.player.actor))
         
-        #self.explosions_handler = explosions.Explosions_Manager()
+        self.explosions_handler = explosions.Explosions_Manager()
         
-    def boom(self):
-        self.explosions_handler.Mortar_Explosion(Point3(0+10*self.count,0-self.count,4))
+        taskMgr.add(self.player_shoot, "Shoot")
+        
+    def create_explosion(self):
+        self.explosions_handler.Small_Explosion(VBase3(0,0,3))
         
     def setKey(self, key, value):
         self.keyMap[key] = value
@@ -121,18 +116,21 @@ class World(DirectObject): #subclassing here is necessary to accept events
     def setMortar(self):
         self.player.set_weapon("Mortar")
         
-    def loadModels(self):
-        """loads models into the world"""
-        pass
         
     def toggle_light(self):
         self.player.toggle_light()
+        
+    def player_shoot(self, task):
+        if not self.paused:
+            if self.keyMap["shoot"]:
+                self.player.shoot(self)
+        return Task.cont
         
     def setupLights(self):
         #ambient light
         self.ambientLight = AmbientLight("ambientLight")
         #four values, RGBA (alpha is largely irrelevent), value range is 0:1
-        self.ambientLight.setColor((.25, .25, .25, 1))
+        self.ambientLight.setColor((.01, .01, .01, 0.1))
         self.ambientLightNP = render.attachNewNode(self.ambientLight)
         #the nodepath that calls setLight is what gets illuminated by the light
         render.setLight(self.ambientLightNP)
@@ -148,9 +146,6 @@ class World(DirectObject): #subclassing here is necessary to accept events
         self.fillLightNP = render.attachNewNode(self.fillLight)
         self.fillLightNP.setHpr(30, 0, 0)
         render.setLight(self.fillLightNP)
-        
-    def setupCollisions(self):
-        base.bullets = render.attachNewNode("bullets")
         
         
     def pause(self):
@@ -182,8 +177,6 @@ class World(DirectObject): #subclassing here is necessary to accept events
         """ Update the AIWorld """
         #print self.enemies[0].distanceToTarget()
         self.AIworld.update()
-        for e in self.enemies:
-            e.updateHeight(self)
         return Task.cont
     
     def resume_game(self):
